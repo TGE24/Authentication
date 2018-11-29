87358d3e3cb6ea5859b3b9247aff1e0a4f3714ff
ThaTruth24 <thankgodegbo@gmail.com>
2018-11-29 01:36:49 +0100

    Authentication Successful

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
-var port = 3000;
+const bcrypt = require("bcrypt");
 var bodyParser = require("body-parser");
+var session = require("express-session");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "test",
+    resave: true,
+    saveUninitialized: false
+  })
+);
 
 //view engine
 app.set("view engine", "ejs");
@@ -27,31 +35,54 @@ var userSchema = new mongoose.Schema({
   psw: String
 });
 
-var User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("signup");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.post("/signup", (req, res) => {
-  var myData = new User(req.body);
+  const { email, psw } = req.body;
+
+  const userData = {
+    psw: bcrypt.hashSync(psw, 5), // we are using bcrypt to hash our password before saving it to the database
+    email
+  };
+  var myData = new User(userData);
   myData
     .save()
     .then(item => {
-      res.render("login");
+      res.status(200).send("signup successful");
     })
     .catch(err => {
       res.status(400).send("Unable to save to database");
     });
 });
 
-app.get("/login", (req, res, next) => {
-  User.find({}, (err, user) => {
-    if (err) throw err;
-    res.render("login");
+app.post("/login", (req, res) => {
+  let { email, psw } = req.body;
+  User.findOne({ email: email }, "psw email", (err, userData) => {
+    if (!err) {
+      let passwordCheck = bcrypt.compareSync(psw, userData.psw);
+      if (passwordCheck) {
+        req.session.user = {
+          email: userData.email,
+          id: userData._id
+        };
+        res.status(200).send("You are logged in, Welcome!");
+      } else {
+        res.status(401).send("incorrect password");
+      }
+    } else {
+      res.status(401).send("invalid login credentials");
+    }
   });
 });
 
 //listen to the selected port
-app.listen(3000);
-console.log(`Server is running on port 3000`);
+app.listen(5000);
+console.log(`Server is running on port 5000`);
